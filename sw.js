1cf466a6ab61aebf607aa725a8b0746e61ffa97d
kmks43307-star <kmks43307@gmail.com>
2026-04-30 18:15:52 +0900

    Change cache name and adjust service worker behavior
    Updated cache name and modified service worker events.

--- sw.js
+++ sw.js
@@ -1,17 +1,13 @@
-const CACHE_NAME = "grit-app-v1";
+const CACHE_NAME = "grit-v2";
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(["./"]);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  // no-op
 });
